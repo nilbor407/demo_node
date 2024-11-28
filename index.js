ee6f6a4c3b71b629971690314fb449cec28303d3
Nilesh <nilbor407@gmail.com>
2024-11-28 18:39:40 +0530

    all

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require("express");
+var session = require("express-session");
 var app = express();
+app.use(session({
+    "secret":"126546",
+    "saveUninitialized":true,
+    "resave":true
+}));
 
 app.get("/",function(req,res){
-res.render("home.ejs");
+    if(!req.session.refresh)
+        req.session.refresh = 1
+    else
+        req.session.refresh += 1
+
+    count = req.session.refresh;
+    res.send("Page Refresh Count = "+count);
 });
 
 app.listen(1000);
